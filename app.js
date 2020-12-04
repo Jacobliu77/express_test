a38147b68f0e3989b5fb49588defd976d15e6718
jacobliu77 <751232478@qq.com>
2020-12-04 16:52:59 +0800

    新建了一个登陆的接口并查询数据库对密码进行校验

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jacob
  * @Date: 2020-12-01 17:25:50
- * @LastEditTime: 2020-12-03 10:58:43
+ * @LastEditTime: 2020-12-04 16:41:43
  * @LastEditors: jacob
  * @Description: express框架下的尝试
  */
@@ -11,6 +11,8 @@ const app = express()
 
 const mysql = require('mysql');
 const md5 = require('md5');
+const bodyParser = require('body-parser');
+
 // console.log(md5('123'));
 ///解决跨域
 app.use((req,res,next)=>{
@@ -21,12 +23,10 @@ app.use((req,res,next)=>{
        next();
     })
  //对于数据库数据查询的相关整理
+
+ app.use(bodyParser.urlencoded({extended:false}));
+
 app.get('/all', (req, res) => {
-       res.header('Access-Control-Allow-Origin', req.headers.origin); //需要显示设置来源
-       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-       res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-       res.header('Access-Control-Allow-Credentials', true); //带cookies
-       next();
         var connection = mysql.createConnection({
              host     : 'localhost',
              user     : 'root',
@@ -43,10 +43,6 @@ app.get('/all', (req, res) => {
     })
 
  app.get('/name', (req, res) => {
-     res.header('Access-Control-Allow-Origin', req.headers.origin); //需要显示设置来源
-     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-     res.header('Access-Control-Allow-Credentials', true); //带cookies
      var connection = mysql.createConnection({
           host     : 'localhost',
           user     : 'root',
@@ -63,11 +59,39 @@ app.get('/all', (req, res) => {
  })
 
  app.post('/login',(req,res)=>{
-     res.header('Access-Control-Allow-Origin', req.headers.origin); //需要显示设置来源
-     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-     res.header('Access-Control-Allow-Credentials', true); //带cookies
+    var sqlpw =''
+    var inpw = ''
+    var connection = mysql.createConnection({
+         host     : 'localhost',
+         user     : 'root',
+         password : 'root',
+         database : 'express_mysql'
+     });
+    connection.connect();
+
+    connection.query("SELECT pwd FROM users WHERE username = '" + req.body.username + "'" , function (error, results, fields) {
+    if (error) throw error;
+    inpw = req.body.pwd
+    if(results[0] && results[0].pwd){
+        sqlpw = results[0].pwd
+
+        if( inpw && sqlpw && inpw === sqlpw){
+            res.send('登陆成功')
+        }else {
+            res.send('用户名或密码有误')
+        }
+        
+    }else {
+        res.send('未查询到相关用户')
+    }
+   
+   
+    
+    });
+    connection.end();
+    
  })
+
 
 app.listen(3000, () => console.log('app listening on port 3000!'))
 
